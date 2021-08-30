65a62b458049c7af3676bc127c6f9c389dd8927c
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-30 18:51:31 +0530

    Add project files.

---
+++ ExtenssionMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExtenssionMethodDemo
+{
+    static class ExtenssionString
+    {
+        public static string UpperCaseFirstLater(this string value)
+        {
+            if(value.Length>0)
+            {
+                char[] arr = value.ToCharArray();
+                arr[0] = char.ToUpper(arr[0]);
+                return new string(arr);
+            }
+            return value;
+        }
+    }
+    class ExtenssionMethod
+    {
+
+        static void Main()
+        {
+            //ExtenssionString EXS = new ExtenssionString();
+            string es = "the world is yours!!!";
+            Console.WriteLine(es.UpperCaseFirstLater());
+        }
+    }
+}
